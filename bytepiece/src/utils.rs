2b06135ac25a377c3bc30e3a252f8d8e1ae2e02c
SunDoge <384813529@qq.com>
2023-10-18 11:20:53 +0800

    add fix for viterbi sampling

--- bytepiece/src/utils.rs
+++ bytepiece/src/utils.rs
@@ -1,7 +1,6 @@
 use once_cell::sync::Lazy;
 use regex::bytes::Regex;
 
-
 pub fn normalize(text: &[u8], max_len: usize) -> Vec<&[u8]> {
     if max_len > 0 {
         let pattern = format!(
@@ -14,4 +13,10 @@ pub fn normalize(text: &[u8], max_len: usize) -> Vec<&[u8]> {
         static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r".*\n+|.+").unwrap());
         RE.find_iter(text).map(|mat| mat.as_bytes()).collect()
     }
+}
+
+#[inline]
+pub fn logsumexp(x: f64, y: f64) -> f64 {
+    let (x, y) = if x < y { (y, x) } else { (x, y) };
+    return x + (1.0 + (y - x).exp()).log2();
 }
