44fc8e5b2ace5693d829299b07c18b868e9de2eb
SunDoge <triplez0@outlook.com>
2024-12-21 10:16:27 +0800

    update scripts

--- bytepiece-py/examples/bench.py
+++ bytepiece-py/examples/bench.py
@@ -1,4 +1,11 @@
-import bytepiece
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "rs-bytepiece",
+#     "bytepiece-py",
+# ]
+# ///
+# import bytepiece
 import timeit
 import bytepiece_py
 import rs_bytepiece
@@ -7,21 +14,22 @@ import rs_bytepiece
 TEXT = "BytePiece是一个Byte-based的Unigram分词器，纯Python实现，更加易读和易拓展。由于采用了新的训练算法，所以压缩率通常比现有Tokenizer更高，同时支持多进程加速训练。此外，它直接操作文本的UTF-8 Bytes，几乎不进行任何的预处理，所以更加纯粹和语言无关。"
 
 
-MODEL = "models/bytepiece_80k.model"
+MODEL = "../models/bytepiece_80k.model"
 
-t1 = bytepiece.Tokenizer(MODEL)
+# t1 = bytepiece.Tokenizer(MODEL)
 t2 = bytepiece_py.Tokenizer(MODEL)
 t3 = rs_bytepiece.Tokenizer(MODEL)
 
-assert t1.encode(TEXT) == t2.encode(TEXT)
-assert t1.decode(t1.encode(TEXT)) == t2.decode(t2.encode(TEXT))
-print(t1.encode(TEXT))
+# assert t1.encode(TEXT) == t2.encode(TEXT)
+# assert t1.decode(t1.encode(TEXT)) == t2.decode(t2.encode(TEXT))
+# print(t1.encode(TEXT))
 print(t2.encode(TEXT))
+print(t3.encode(TEXT))
 
 
-print('bytepiece:')
-print(timeit.timeit("t1.encode(TEXT)", globals=globals(), number=10000))
-print('bytepiece-py (ours)')
+# print('bytepiece:')
+# print(timeit.timeit("t1.encode(TEXT)", globals=globals(), number=10000))
+print("bytepiece-py (ours)")
 print(timeit.timeit("t2.encode(TEXT)", globals=globals(), number=10000))
-print('rs-bytepiece')
+print("rs-bytepiece")
 print(timeit.timeit("t3.encode(TEXT)", globals=globals(), number=10000))
--- scripts/download_model.py
+++ scripts/download_model.py
@@ -1,3 +1,11 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "requests",
+# ]
+# ///
+
+
 import argparse
 import os
 import requests
@@ -5,15 +13,15 @@ from pathlib import Path
 from zipfile import ZipFile
 
 MODELS = [
-    'bytepiece_80k',
-    'bytepiece_160k',
+    "bytepiece_80k",
+    "bytepiece_160k",
 ]
 BASE_URL = "https://github.com/bojone/bytepiece/raw/main/models/"
 DEST_DIR = "models"
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--model', choices=MODELS, default=MODELS[0])
+parser.add_argument("-m", "--model", choices=MODELS, default=MODELS[0])
 
 args = parser.parse_args()
 
@@ -21,7 +29,7 @@ dest_dir = Path(DEST_DIR)
 dest_dir.mkdir(exist_ok=True)
 
 model_name: str = args.model
-model_filename = model_name + '.zip'
+model_filename = model_name + ".zip"
 model_url = BASE_URL + model_filename
 model_file_path = dest_dir / model_filename
 
@@ -30,10 +38,10 @@ print(model_url)
 
 if not model_file_path.exists():
     resp = requests.get(model_url)
-    with model_file_path.open('wb') as f:
+    with model_file_path.open("wb") as f:
         f.write(resp.content)
 else:
-    print(f'{model_file_path} exists')
+    print(f"{model_file_path} exists")
 
 with ZipFile(model_file_path) as zip_file:
     zip_file.extractall(dest_dir)
